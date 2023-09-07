e96d349e592728905a7b6b6e026b278ef353cb67
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2023-09-07 21:00:24 +0700

    fix website and change content

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-document.getElementById("active");
+const navbarButton = document.querySelector('.navbar-nav');
+const hm = document.querySelector('#hamburger-menu');
+// button burger ketika di klik
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    navbarButton.classList.toggle('active');
+    e.preventDefault();
+}
+
+document.addEventListener('click',function(e) {
+    if(!hm.contains(e.target) && !navbarButton.contains(e.target)) {
+        navbarButton.classList.remove('active');
+    }
+})
