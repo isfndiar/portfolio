c47164b42540d8de42147bbc390f0e1554d3f392
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2023-09-30 19:10:11 +0700

    feat : create light mode 10% and if we scroll to bottom navbar will be gone

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const navbarButton = document.querySelector('.navbar-nav');
 const hm = document.querySelector('#hamburger-menu');
+
 // button burger ketika di klik
 document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarButton.classList.toggle('active');
@@ -9,5 +10,43 @@ document.querySelector('#hamburger-menu').onclick = (e) => {
 document.addEventListener('click',function(e) {
     if(!hm.contains(e.target) && !navbarButton.contains(e.target)) {
         navbarButton.classList.remove('active');
+
     }
 })
+
+
+const setBg = document.querySelector('#set-bg')
+const userPhoto = document.querySelector('#photo-profile')
+const navbar = document.querySelector('.navbar')
+const navbarLogo = document.querySelector('.navbar-logo')
+
+
+setBg.addEventListener('click', function (e) {
+    const linkCss = document.querySelector('.hai')
+    linkCss.href.toggle = 'src/bang.css'
+    // DarkToLight()
+    // heroSection()
+    e.preventDefault()
+
+})
+
+const prevScroll = window.pageYOffset;
+window.addEventListener('scroll', function() {
+    this.window.pageYOffset
+})
+
+
+
+function DarkToLight() {
+    document.body.classList.toggle('white')
+    userPhoto.classList.toggle('bg-profile')
+    navbar.classList.toggle('white')
+    hm.classList.toggle('color-black')
+    navbarLogo.classList.toggle('color-black')
+    navbarButton.classList.toggle('white')
+    for (let acnhor of navbarButton.children) {
+        
+        acnhor.classList.toggle('white')
+    }
+}
+
